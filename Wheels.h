f9a0271104c8dfd2307b0dedb256938712f3bea0
xjms111 <a.dejewski07@gmail.com>
2026-03-18 12:33:17 +0100

    Update Wheels.h

--- Wheels.h
+++ Wheels.h
@@ -1,17 +1,41 @@
+/* 
+ * prosta implementacja klasy obsługującej 
+ * silniki pojazdu za pośrednictwem modułu L298
+ *
+ * Sterowanie odbywa się przez:
+ * 1)  powiązanie odpowiednich pinów I/O Arduino metodą attach() 
+ * 2)  ustalenie prędkości setSpeed*()
+ * 3)  wywołanie funkcji ruchu
+ *
+ * TODO:
+ *  - zabezpieczenie przed ruchem bez attach()
+ *  - ustawienie domyślnej prędkości != 0
+ */
+
+
+#include <Arduino.h>
+
+
 #ifndef Wheels_h
 #define Wheels_h
 
-#include <Arduino.h>
+
 
 class Wheels {
     public: 
         Wheels();
-
+        /*
+         *  pinForward - wejście "naprzód" L298
+         *  pinBack    - wejście "wstecz" L298
+         *  pinSpeed   - wejście "enable/PWM" L298
+         */
         void attachRight(int pinForward, int pinBack, int pinSpeed);
         void attachLeft(int pinForward, int pinBack, int pinSpeed);
         void attach(int pinRightForward, int pinRightBack, int pinRightSpeed,
                     int pinLeftForward, int pinLeftBack, int pinLeftSpeed);
-
+        /*
+         *  funkcje ruchu
+         */
         void forward();
         void forwardLeft();
         void forwardRight();
@@ -21,12 +45,14 @@ class Wheels {
         void stop();
         void stopLeft();
         void stopRight();
-
+        /*
+         *  ustawienie prędkości obrotowej (przez PWM)
+         *   - minimalna efektywna wartość 60
+         *      może zależeć od stanu naładowania baterii
+         */
         void setSpeed(uint8_t);
         void setSpeedRight(uint8_t);
         void setSpeedLeft(uint8_t);
-
-        // NOWE
         void goForward(int cm);
         void goBack(int cm);
 
@@ -34,5 +60,7 @@ class Wheels {
         int pinsRight[3];
         int pinsLeft[3];
 };
+
+
 
 #endif
