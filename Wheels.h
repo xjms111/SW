caedd72f4f4c2837e8c59014342d98964b24f8d4
xjms111 <a.dejewski07@gmail.com>
2026-03-18 09:33:48 +0100

    Create Wheels.h

---
+++ Wheels.h
@@ -0,0 +1,38 @@
+#ifndef Wheels_h
+#define Wheels_h
+
+#include <Arduino.h>
+
+class Wheels {
+    public: 
+        Wheels();
+
+        void attachRight(int pinForward, int pinBack, int pinSpeed);
+        void attachLeft(int pinForward, int pinBack, int pinSpeed);
+        void attach(int pinRightForward, int pinRightBack, int pinRightSpeed,
+                    int pinLeftForward, int pinLeftBack, int pinLeftSpeed);
+
+        void forward();
+        void forwardLeft();
+        void forwardRight();
+        void back();
+        void backLeft();
+        void backRight();
+        void stop();
+        void stopLeft();
+        void stopRight();
+
+        void setSpeed(uint8_t);
+        void setSpeedRight(uint8_t);
+        void setSpeedLeft(uint8_t);
+
+        // NOWE
+        void goForward(int cm);
+        void goBack(int cm);
+
+    private: 
+        int pinsRight[3];
+        int pinsLeft[3];
+};
+
+#endif
