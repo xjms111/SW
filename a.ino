813bd8b09cbd31313fafc85d068ee36cde33de71
xjms111 <a.dejewski07@gmail.com>
2026-03-18 12:33:03 +0100

    Update a.ino

--- a.ino
+++ a.ino
@@ -1,40 +1,19 @@
 #include "Wheels.h"
 
+
+
 Wheels w;
 volatile char cmd;
 
-// zmienne enkoderów
-volatile long encoderLeft = 0;
-volatile long encoderRight = 0;
-
-// ISR (przerwania)
-void encoderLeftISR()
-{
-  encoderLeft++;
-}
-
-void encoderRightISR()
-{
-  encoderRight++;
-}
-
 void setup() {
-  w.attach(7,8,5,12,11,10);
-
+  // put your setup code here, to run once:
+  w.attach(7,12, 3,9,11,5);
+  
   Serial.begin(9600);
-
-  // konfiguracja enkoderów
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-
-  attachInterrupt(digitalPinToInterrupt(2), encoderLeftISR, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), encoderRightISR, RISING);
-
   Serial.println("Forward: WAD");
   Serial.println("Back: ZXC");
   Serial.println("Stop: S");
-  Serial.println("Distance forward: F");
-  Serial.println("Distance back: B");
+
 }
 
 void loop() {
@@ -50,15 +29,14 @@ void loop() {
       case 'z': w.backLeft(); break;
       case 'c': w.backRight(); break;
       case 's': w.stop(); break;
-
       case '1': w.setSpeedLeft(75); break;
       case '2': w.setSpeedLeft(200); break;
       case '9': w.setSpeedRight(75); break;
       case '0': w.setSpeedRight(200); break;
       case '5': w.setSpeed(100); break;
 
-      case 'f': w.goForward(50); break;
-      case 'b': w.goBack(50); break;
+      case'f':w.goForward(10); break;
+      case'b':w.goBack(10); break;
     }
   }
 }
